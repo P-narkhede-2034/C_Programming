a376fe38fc7b43c1d5c17750b4414e0214852e24
Pranav <pranavn2034@gmail.com>
2025-11-23 22:20:03 +0530

    Update LBassitment11.4.c

--- Assignments/Assignment_11/LBassitment11.4.c
+++ Assignments/Assignment_11/LBassitment11.4.c
@@ -1,25 +1,43 @@
-//4. Write a program which accept range from user and return addition of all even
-//numbers in between that range. (Range should contains positive numbers only)
+////////////////////////////////////////////////////////////////////////
+//  QUESTION : Write a program which accept range from user and return 
+//             addition of all even numbers in between that range.
+//             (Range should contains positive numbers only)
+////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////
+//    REQUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
 #include<stdio.h>
 
+////////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : int RangeDisplayEven(int ino1 , int ino2)
+//      DESCRIPTION    : This function returns addition of all even numbers 
+//                       between the given range.
+//      INPUT          : ino1, ino2
+//      OUTPUT         : int
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+////////////////////////////////////////////////////////////////////////
 int RangeDisplayEven(int ino1 ,int ino2)
 {
     int i = 0;
-     int j = 0;
-     for( i = ino1 ; i <= ino2 ; i++)
-     {
+    int j = 0;
+
+    for( i = ino1 ; i <= ino2 ; i++)
+    {
         if(i % 2 == 0)
         {
-             j = j + i;
+            j = j + i;
         }
-       
-     }
-     return j;
+    }
+    return j;
 }
+
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
 int main()
 {
-    int iValue1 = 0,iValue2 = 0, isum = 0;
+    int iValue1 = 0, iValue2 = 0, isum = 0;
 
     printf("Enter the first number :");
     scanf("%d",&iValue1);
@@ -27,8 +45,8 @@ int main()
     printf("Enter the  secound number :");
     scanf("%d",&iValue2);
 
-
-    isum =RangeDisplayEven(iValue1 , iValue2);
+    isum = RangeDisplayEven(iValue1 , iValue2);
     printf("sum is all even addition : %d",isum);
+
     return 0;
 }
