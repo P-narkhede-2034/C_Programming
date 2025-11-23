24150e380c29f38ee741a012249c2eaa20018abd
Pranav <pranavn2034@gmail.com>
2025-11-23 21:58:08 +0530

    Update LBassitment1.1.c

--- Assignments/Assignment_1/LBassitment1.1.c
+++ Assignments/Assignment_1/LBassitment1.1.c
@@ -1,5 +1,18 @@
+////////////////////////////////////////////////////////////////////////
+//    REQUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 
+////////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : int Divide(int iNo1 , int iNo2)
+//      DESCRIPTION    : This function performs division of two integers 
+//                        and returns the result. If denominator is zero,
+//                        it returns -1 as an error indication.
+//      INPUT          : iNo1 , iNo2 
+//      OUTPUT         : Integer 
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+//      DATE           : 19/11/2025
+////////////////////////////////////////////////////////////////////////
 int Divide(int iNo1 , int iNo2)
 {
     int iAns = 0;
@@ -11,11 +24,17 @@ int Divide(int iNo1 , int iNo2)
     return iAns;
 }
 
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
 int main()
 {
     int iValue1 = 15 ,iValue2 = 5;
     int iRet = 0;
+
     iRet = Divide(15,5);
+
     printf("Division is : %d",iRet);
+
     return 0;
 }
