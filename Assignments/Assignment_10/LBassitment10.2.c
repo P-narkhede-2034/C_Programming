2599942c985f08e501ff5c52daf748ae38ef09c4
Pranav <pranavn2034@gmail.com>
2025-11-23 22:06:05 +0530

    Update LBassitment10.2.c

--- Assignments/Assignment_10/LBassitment10.2.c
+++ Assignments/Assignment_10/LBassitment10.2.c
@@ -1,16 +1,33 @@
-// 2. Write a program which accept width & height of rectangle from user and calculate
-//its area. (Area = Width * Height)
+////////////////////////////////////////////////////////////////////////
+//  QUESTION : Write a program which accept width & height of rectangle 
+//             from user and calculate its area. 
+//             (Area = Width * Height)
+////////////////////////////////////////////////////////////////////////
 
-
+////////////////////////////////////////////////////////////////////////
+//    REQUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
 #include<stdio.h>
 
+////////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : double Rectarea(float width , float height)
+//      DESCRIPTION    : This function accepts width and height of a 
+//                        rectangle and returns its area.
+//      INPUT          : width, height (floating-point values)
+//      OUTPUT         : double (calculated area)
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+//      DATE           : 
+////////////////////////////////////////////////////////////////////////
 double Rectarea(float width , float height)
 {
-   
     double area = 0;
     area = width * height ;
     return area;
 }
+
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
 int main()
 {
     float  iValue1 = 0 , iValue2 = 0;
@@ -23,6 +40,8 @@ int main()
     scanf("%f",&iValue2);
 
     iret = Rectarea(iValue1,iValue2);
-    printf("area of reactangle is : %lf",iret);
+
+    printf("Area of rectangle is : %lf",iret);
+
     return 0;
 }
