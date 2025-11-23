9716402f21a2eb04b3ba835b256a77f9863e11f6
Pranav <pranavn2034@gmail.com>
2025-11-23 22:09:57 +0530

    Update LBassitment10.5.C

--- Assignments/Assignment_10/LBassitment10.5.C
+++ Assignments/Assignment_10/LBassitment10.5.C
@@ -1,23 +1,44 @@
-//5. Write a program which accept area in square feet and convert it into square
-//meter. (1 square feet = 0.0929 Square meter)
+////////////////////////////////////////////////////////////////////////
+//  QUESTION : Write a program which accept area in square feet and 
+//             convert it into square meter. 
+//             (1 Square Feet = 0.0929 Square Meter)
+////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////
+//    REQUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
 #include<stdio.h>
 
-double  Squaremeter(int ino)
+////////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : double SquareMeter(int ino)
+//      DESCRIPTION    : This function converts area from square feet 
+//                        into square meters.
+//      INPUT          : ino
+//      OUTPUT         : double
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+//      DATE           : 
+////////////////////////////////////////////////////////////////////////
+double SquareMeter(int ino)
 {
-     double meter = 0;
+     double meter = 0.0;
      meter = ino * 0.0929;
      return meter;
 }
+
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
 int main()
 {
-    int  iValue1 = 0 ;
-    double iret = 0.0; 
+    int iValue1 = 0;
+    double iRet = 0.0;
 
-    printf("Enter kilomeater:");
+    printf("Enter area in square feet : ");
     scanf("%d",&iValue1);
 
-    iret = Squaremeter(iValue1);
-    printf("area of reactangle is : %lf",iret);
+    iRet = SquareMeter(iValue1);
+
+    printf("Area in square meter is : %lf", iRet);
+
     return 0;
 }
