f73fb64188c7e31e563033348cfe52ce6efd9158
p-narkhede-2034 <pranavn2034@gmail.com>
2025-11-23 21:48:31 +0530

    ass 31 c

---
+++ Assignments/Assignment_31/program1.c
@@ -0,0 +1,67 @@
+///////////////////////////////////////////////////////////////////////
+//2. Accept number of rows and number of columns from user and display
+  //below pattern.
+
+//OUTPUT
+/* 
+Enter the rows
+4
+Enter the colums
+4
+*
+*       *
+*       *       *
+*       *       *       *
+*/  
+///////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////
+//    REUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void display(int iRow , int iCol)
+{  
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+             for(j = 1; j <= iCol ; j++)
+            {
+                if( i >= j)
+                {
+                  printf("*\t");
+                }
+            }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+    
+    printf("Enter the rows\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the colums\n");
+    scanf("%d",&iValue2);
+
+    display(iValue1,iValue2);
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : void display(int iRow , int iCol)
+//      DESCRIPTION    : DIPLAY PATTERN 
+//      INPUT          : iValue1,iValue2
+//      OUPUT          : VOID
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+//      DATE           : 23/11/2025
+///////////////////////////////////////////////////////////////////////
---
+++ Assignments/Assignment_31/program5.c
@@ -0,0 +1,71 @@
+///////////////////////////////////////////////////////////////////////
+//2. Accept number of rows and number of columns from user and display
+  //below pattern.
+
+//OUTPUT
+/* 
+Enter the rows
+4
+Enter the colums
+4
+1       2       3       4
+        2       3       4
+                3       4
+                        4
+*/  
+///////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////
+//    REUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void display(int iRow , int iCol)
+{  
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+             for(j = 1; j <= iCol  ; j++)
+            {
+               if (i > j)
+               {
+                printf("\t");
+               }
+               else
+               {
+                printf("%d\t",j);
+               }
+            }
+        printf("\n");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+    
+    printf("Enter the rows\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the colums\n");
+    scanf("%d",&iValue2);
+
+    display(iValue1,iValue2);
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : void display(int iRow , int iCol)
+//      DESCRIPTION    : DIPLAY PATTERN 
+//      INPUT          : iValue1,iValue2
+//      OUPUT          : VOID
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+//      DATE           : 23/11/2025
+///////////////////////////////////////////////////////////////////////
