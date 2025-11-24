d9c7c84100f6164d081f9a377286bf84ce184586
Pranav <pranavn2034@gmail.com>
2025-11-24 15:39:13 +0530

    Update LBassitment14.1.c

--- Assignments/Assignment_14/LBassitment14.1.c
+++ Assignments/Assignment_14/LBassitment14.1.c
@@ -1,7 +1,21 @@
-// 1. Write a program which accept number from user and display its digits in reverse
-//order.
+////////////////////////////////////////////////////////////////////////
+//  QUESTION : Write a program which accept number from user and 
+//             display its digits in reverse order.
+////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+//    REQUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
 #include<stdio.h>
 
+////////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : void DisplayDigit(int ino)
+//      DESCRIPTION    : This function prints digits of a number 
+//                       in reverse order.
+//      INPUT          : ino
+//      OUTPUT         : void
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+////////////////////////////////////////////////////////////////////////
 void DisplayDigit(int ino)
 {
     int idigit = 0;
@@ -21,15 +35,16 @@ void DisplayDigit(int ino)
      printf("-------------------------------------------------\n");
 }
 
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
 int main()
 {
-
     int iValue = 0;
 
     printf("enter the no :");
     scanf("%d",&iValue);
     DisplayDigit(iValue);
-    
 
     return 0;
 }
