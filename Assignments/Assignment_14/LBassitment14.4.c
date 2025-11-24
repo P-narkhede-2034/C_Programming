9e257c8971a4e797593371f679e2e05993ceb4d4
Pranav <pranavn2034@gmail.com>
2025-11-24 15:41:16 +0530

    Update LBassitment14.4.c

--- Assignments/Assignment_14/LBassitment14.4.c
+++ Assignments/Assignment_14/LBassitment14.4.c
@@ -1,26 +1,36 @@
-//4. Write a program which accept number from user and count frequency of 4 in it.
+////////////////////////////////////////////////////////////////////////
+//  QUESTION : Write a program which accept number from user and count
+//             frequency of 4 in it.
+////////////////////////////////////////////////////////////////////////
 
-
+////////////////////////////////////////////////////////////////////////
+//    REQUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdbool.h>
 
+////////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : void checkbool(int ino)
+//      DESCRIPTION    : This function counts frequency of digit 4.
+//      INPUT          : ino
+//      OUTPUT         : void
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+////////////////////////////////////////////////////////////////////////
 void checkbool(int ino)
 {
     int idigit = 0;
-    int arr[100];  // array to store digits
+    int arr[100];  
     int i = 0; 
-    int frequency = 0;    // index
+    int frequency = 0;   
 
     while (ino != 0)
     {
-        idigit = ino % 10;   // extract last digit
-        arr[i] = idigit;     // store in array
-        ino = ino / 10;      // remove last digit
-        i++;                 // move to next index
+        idigit = ino % 10;   
+        arr[i] = idigit;     
+        ino = ino / 10;      
+        i++;                
     }
 
-
- 
     for (int j = 0; j < i; j++)
     {
         if (arr[j] == 4)
@@ -32,6 +42,9 @@ void checkbool(int ino)
     printf("the no of 4 frequency is :%d",frequency);
 }
 
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
 int main()
 {
     int iValue = 0;
