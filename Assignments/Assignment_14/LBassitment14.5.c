a462ec1d837c01cad8c0d44de8af27f341155c9d
Pranav <pranavn2034@gmail.com>
2025-11-24 15:42:24 +0530

    Update LBassitment14.5.c

--- Assignments/Assignment_14/LBassitment14.5.c
+++ Assignments/Assignment_14/LBassitment14.5.c
@@ -1,26 +1,36 @@
-// . Write a program which accept number from user and count frequency of such a
-//digits which are less than 6.
+////////////////////////////////////////////////////////////////////////
+//  QUESTION : Write a program which accept number from user and count
+//             frequency of such digits which are less than 6.
+////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////
+//    REQUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <stdbool.h>
 
-void checkbool(int ino)
+////////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : void CountLessThanSix(int ino)
+//      DESCRIPTION    : This function counts digits less than 6.
+//      INPUT          : ino
+//      OUTPUT         : void
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+////////////////////////////////////////////////////////////////////////
+void CountLessThanSix(int ino)
 {
     int idigit = 0;
-    int arr[100];  // array to store digits
+    int arr[100];  
     int i = 0; 
-    int frequency = 0;    // index
+    int frequency = 0;    
 
     while (ino != 0)
     {
-        idigit = ino % 10;   // extract last digit
-        arr[i] = idigit;     // store in array
-        ino = ino / 10;      // remove last digit
-        i++;                 // move to next index
+        idigit = ino % 10;   
+        arr[i] = idigit;     
+        ino = ino / 10;      
+        i++;                 
     }
 
-
- 
     for (int j = 0; j < i; j++)
     {
         if (arr[j] < 6)
@@ -29,16 +39,19 @@ void checkbool(int ino)
         }
     }
      
-    printf("the no  frequency less than 6 is :%d",frequency);
+    printf("the no frequency less than 6 is : %d",frequency);
 }
 
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
 int main()
 {
     int iValue = 0;
     printf("Enter number: ");
     scanf("%d", &iValue);
 
-    checkbool(iValue);
+    CountLessThanSix(iValue);
 
     return 0;
 }
