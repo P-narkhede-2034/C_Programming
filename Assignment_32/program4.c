87a1d2c15dd09b2ffddfc7093a330eb7e106e87f
p-narkhede-2034 <pranavn2034@gmail.com>
2025-11-25 17:53:10 +0530

    assignment 32 c

---
+++ Assignment_32/program4.c
@@ -0,0 +1,86 @@
+///////////////////////////////////////////////////////////////////////
+//4. Accept number of rows and number of columns from user and display
+  //below pattern.
+
+//OUTPUT
+/* 
+Enter the rows
+6
+Enter the colums
+6
+*       *       *       *       *       *
+*       *       $       $       $       *
+*       #       *       $       $       *
+*       #       #       *       $       *
+*       #       #       #       *       *
+*       *       *       *       *       *
+*/  
+///////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////
+//    REUIRED HEADER FILE 
+////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void display(int iRow , int iCol)
+{  
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+             for(j = 1; j <= iCol  ; j++)
+            {
+
+                if(i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("*\t");
+            }
+           
+            else if(j < i)
+            {
+                printf("#\t");
+            }
+           
+            else if(j > i)
+            {
+                printf("$\t");
+            }
+            else      
+            {
+                printf("*\t");
+            }
+            }
+            
+         printf("\n");
+           
+}
+}
+
+////////////////////////////////////////////////////////////////////////
+//  ENTRY POINT FUNCTION FOR THE APPLICATION
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 , iValue2 = 0;
+    
+    printf("Enter the rows\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the colums\n");
+    scanf("%d",&iValue2);
+
+    display(iValue1,iValue2);
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////
+//      FUNCTION NAME  : void display(int iRow , int iCol)
+//      DESCRIPTION    : DIPLAY PATTERN 
+//      INPUT          : iValue1,iValue2
+//      OUPUT          : VOID
+//      AUTHOR         : PRANAV MANOJ NARKHEDE
+//      DATE           : 25/11/2025
+///////////////////////////////////////////////////////////////////////
